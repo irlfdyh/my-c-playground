3e283ee66772e4971284879292f8a6fe906da7b8
irshalfir <irshalfir@gmail.com>
2020-10-10 11:28:15 +0700

    add boolean and relational

---
+++ BooleanAndRelational.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int uang = 6000;
+    int hujan = 0;
+
+    printf("uang>5000 dan tidak hujan : %d\n", (uang>5000)&& (!hujan));
+    
+    hujan = 1;
+    printf("uang>5000 dan tidak hujan: %d \n",  (uang>5000) && (!hujan));
+
+    return 0;
+}
