2187e693d1a73fbd464c8ae275a17115dc96e0e7
irshalfir <irshalfir@gmail.com>
2020-11-08 20:10:17 +0700

    add my hands-on program from college study

---
+++ CollegeStudentScoreProgram.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    char collegeStudentName[20];
+    float quizScore, utsScore, uasScore, taskScore, finalScore;
+
+    printf("Nama Mahasiswa  : "); scanf("%s", &collegeStudentName);
+    printf("Nilai Quiz      : "); scanf("%f", &quizScore);
+    printf("Nilai Tugas     : "); scanf("%f", &taskScore);
+    printf("Nilai UTS       : "); scanf("%f", &utsScore);
+    printf("Nilai UAS       : "); scanf("%f", &uasScore);
+
+    finalScore = (0.1*quizScore) + (0.2*taskScore) + (0.3*utsScore) + (0.4*uasScore);
+
+    if (finalScore >= 0.00 && finalScore <= 30.00) {
+        printf("\n%s Mendapat Nilai %.2f dengan Huruf Mutu E\n\n", collegeStudentName, finalScore);
+    } else if (finalScore >= 31.00 && finalScore <= 45.00) {
+        printf("\n%s Mendapat Nilai %.2f dengan Huruf Mutu D\n\n", collegeStudentName, finalScore);
+    } else if (finalScore >= 46.00 && finalScore <= 65.00) {
+        printf("\n%s Mendapat Nilai %.2f dengan Huruf Mutu C\n\n", collegeStudentName, finalScore);
+    } else if (finalScore >= 66.00 && finalScore <= 79.00) {
+        printf("\n%s Mendapat Nilai %.2f dengan Huruf Mutu B\n\n", collegeStudentName, finalScore);
+    } else if (finalScore >= 80.00 && finalScore <= 100.00) {
+        printf("\n%s Mendapat Nilai %.2f dengan Huruf Mutu A\n\n", collegeStudentName, finalScore);
+    } else if (finalScore > 100.00) {
+        printf("\nMustahil memiliki nilai %.2f\n", finalScore);
+    } else {
+        printf("\nMengulang dari SD !!!!\n");
+    }
+
+    return 0;
+}
---
+++ WaterProviderProgram.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define priceTypeA 15000
+#define priceTypeB 20000
+#define priceTypeC 25000
+
+int main() {
+
+    char customerName, customerType;
+    float pricePerCubic, paymentTotal, usedWaterVolume;
+
+    printf("Nama Pelangan              = "); scanf("%s", &customerName);
+    printf("Tipe pelanggan A/B/C       = "); scanf("%s", &customerType);
+
+    customerType = toupper(customerType);
+
+    switch (customerType) {
+        case 'A' : pricePerCubic = priceTypeA;
+        break;
+        case 'B' : pricePerCubic = priceTypeB;
+        break;
+        case 'C' : pricePerCubic = priceTypeC;
+        break;
+        default  : 
+            customerType = 'Z';
+            printf("--------------------------------------------------\n");
+            printf("Tipe pelanggan tidak ada!!\n");
+        break;
+    }
+
+    if (customerType != 'Z') {
+        printf("Harga per kubik            = Rp.%.2f\n", pricePerCubic);
+        printf("Volume air yang digunakan  = "); scanf("%f", &usedWaterVolume);
+        printf("--------------------------------------------------\n");
+
+        if (customerType == 'A') {
+            paymentTotal = priceTypeA * usedWaterVolume;
+        } else if (customerType == 'B') {
+            paymentTotal = priceTypeB * usedWaterVolume;
+        } else if (customerType == 'C') {
+            paymentTotal = priceTypeC * usedWaterVolume;
+        }
+        
+        printf("TOTAL BAYAR                = Rp.%.2f\n", paymentTotal);
+    } 
+
+    return 0;
+}
