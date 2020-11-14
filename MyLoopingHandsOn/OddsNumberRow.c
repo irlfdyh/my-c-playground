b63bdb33513a83cda444c110ebf1015dd193b47e
irshalfir <irshalfir@gmail.com>
2020-11-14 12:06:40 +0700

    refactor folder name and create new tasks

---
+++ MyLoopingHandsOn/OddsNumberRow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;
+
+    for (number = 20; number <= 100; number++) {
+        if (number % 2 == 0) {
+            if (number % 5 == 0) {
+                printf("%d ", number);
+            }
+        }
+    }
+
+    return 0;
+}
