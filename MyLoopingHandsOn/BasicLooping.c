884e042a34b4cbc68637eb92ec170eea7d15df73
irshalfir <irshalfir@gmail.com>
2020-11-23 20:27:48 +0700

    add more my hands on

---
+++ MyLoopingHandsOn/BasicLooping.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    // Use for to print odds number
+    for (i = 10; i <= 20; i += 2) {
+        printf("%d\n", i);
+    }
+
+    // Use do while to print odds number
+    i = 10;
+    do {
+        printf("%d\n", i);
+        i += 2;
+    } while (i <= 20)
+
+    return 0;
+}
