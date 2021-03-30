e42ca76fc7d367f6e6283f0d975832e97ac36361
Your Name <irshalfir@gmail.com>
2021-03-30 10:24:21 +0700

    delete space at sorting program

--- university-hands-on/s2/2-sorting/sort-prg-one.c
+++ university-hands-on/s2/2-sorting/sort-prg-one.c
@@ -14,7 +14,6 @@ int rqdt, cln[20];
 int main() {
     get_usr_rqd();
     get_cln_num();
-
     switch (get_srt_opt()) {
         case 0 : srt_dsc(); 
             break;
@@ -23,58 +22,44 @@ int main() {
         default : printf("Exception here");
             break;
     }
-
     prt_rslt();
-    
     return 0;
 }
 
 void get_usr_rqd() {
-    
     do {
         printf("\nMasukkan jumlah data yang akan di urutkan (maksimal 20) : ");
         scanf("%d", &rqdt); fflush(stdin);
     } while (rqdt > 20);
-
 }
 
 void get_cln_num() {
-
     int i;
-
     printf("\nMasukkan angka sebanyak %d\n", rqdt);
     for (i = 0; i < rqdt; i++) {
         printf("%d. ", i+1);
         scanf("%d", &cln[i]);
         fflush(stdin);
     }
-
 }
 
 int get_srt_opt() {
-
     int srt;
-
     puts("\nOpsi pengurutan :");
     puts("0 [Descending]");
     puts("1 [Ascending]");
     printf("Pilihan : ");
     scanf("%d", &srt); fflush(stdin);
-
     if (srt > 1 || srt < 0) {
         get_srt_opt();
     } else {
         return srt;
     }
-
 }
 
 void srt_dsc() {
-
     int cntr, chl_cntr, swp;
-
     puts("\ndescending sort");    
-
     for (cntr = 0; cntr < rqdt - 1; cntr++) {
         for (chl_cntr = 0; chl_cntr < rqdt - 1; chl_cntr++) {
             if (cln[chl_cntr] < cln[chl_cntr + 1]) {
@@ -87,11 +72,8 @@ void srt_dsc() {
 }
 
 void srt_asc() {
-
     int cntr, chl_cntr, swp;
-
     puts("\nascending sort");
-
     for (cntr = 0; cntr < rqdt - 1; cntr++) {
         for (chl_cntr = 0; chl_cntr < rqdt - 1; chl_cntr++) {
             if (cln[chl_cntr] > cln[chl_cntr + 1]) {
@@ -104,12 +86,9 @@ void srt_asc() {
 }
 
 void prt_rslt() {
-
     int i;
-
     puts("\nData yang telah di urutkan");
     for (i = 0; i < rqdt; i++) {
         printf("%d. %d\n", i+1, cln[i]);
     }
-
 }
--- university-hands-on/s2/2-sorting/sort-prg-two.c
+++ university-hands-on/s2/2-sorting/sort-prg-two.c
@@ -10,55 +10,44 @@ void bbl_srt(int typ);
 void slc_srt(int typ);
 
 // Global variable
-int rqdt, cln[20];
+int rqdt, cln[30];
 
 int main() {
     get_usr_rqd();
     get_cln_num();
-
     switch (get_srt_algr()) {
         case 0  : bbl_srt(get_srt_opt()); break;
         case 1  : slc_srt(get_srt_opt()); break;
         default : printf("Exception here"); break;
     }
-
     prt_rslt();
-    
     return 0;
 }
 
 void get_usr_rqd() {
-    
     do {
         printf("\nMasukkan jumlah data yang akan di urutkan (maksimal 20) : ");
         scanf("%d", &rqdt); fflush(stdin);
-    } while (rqdt > 20);
-
+    } while (rqdt > 30);
 }
 
 void get_cln_num() {
-
     int i;
-
     printf("\nMasukkan angka sebanyak %d\n", rqdt);
     for (i = 0; i < rqdt; i++) {
         printf("%d. ", i+1);
         scanf("%d", &cln[i]);
         fflush(stdin);
     }
-
 }
 
 int get_srt_algr() {
-
     int algr;
-
     puts("\nOpsi algoritma :");
     puts("0 [Bubble Sort]");
     puts("1 [Selection Sort]");
     printf("Pilihan : ");
     scanf("%d", &algr); fflush(stdin);
-
     if (algr > 1 || algr < 0) {
         get_srt_algr();
     } else {
@@ -67,15 +56,12 @@ int get_srt_algr() {
 }
 
 int get_srt_opt() {
-
     int srt;
-
     puts("\nOpsi pengurutan :");
     puts("0 [Descending]");
     puts("1 [Ascending]");
     printf("Pilihan : ");
     scanf("%d", &srt); fflush(stdin);
-
     if (srt > 1 || srt < 0) {
         get_srt_opt();
     } else {
@@ -84,9 +70,7 @@ int get_srt_opt() {
 }
 
 void bbl_srt(int typ) {
-
     int cntr, chl_cntr, swp;
-
     if (typ == 0) {
         puts("\nmemproses bubble sort descending...");
         for (cntr = 0; cntr < rqdt - 1; cntr++) {
@@ -110,13 +94,10 @@ void bbl_srt(int typ) {
             }
         }
     } 
-
 }
 
 void slc_srt(int typ) {
-
     int cntr, chl_cntr, swp;
-
     if (typ == 0) {
         puts("\nmemproses selection sort descending...");
         for (cntr = 0; cntr < rqdt - 1; cntr++) {
@@ -140,16 +121,12 @@ void slc_srt(int typ) {
             }
         }
     }
-
 }
 
 void prt_rslt() {
-
     int i;
-
     puts("\nData yang telah di urutkan");
     for (i = 0; i < rqdt; i++) {
         printf("%d. %d\n", i+1, cln[i]);
     }
-
 }
