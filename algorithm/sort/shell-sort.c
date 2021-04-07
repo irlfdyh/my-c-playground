b761d23f8f88bf0987a5e2ba79610d3b3ecfdd44
Your Name <irshalfir@gmail.com>
2021-04-07 22:57:23 +0700

    add shell sort algorithm

---
+++ algorithm/sort/shell-sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void asc_shell_sort(int arr[], int n);
+void dsc_shell_sort(int arr[], int n);
+void print_array(int arr[], int n);
+
+int main() {
+
+    int arr[] = { 23, 53, 2, 64, 12, 22 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    asc_shell_sort(arr, n);
+
+    print_array(arr, n);
+
+    return 0;
+}
+
+void asc_shell_sort(int arr[], int n) {
+
+    int i, j, gap, temp;
+
+    for (gap = n/2; gap > 0; gap /=2) {
+        for (i = gap; i < n; i++) {
+            temp = arr[i];
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+
+}
+
+void print_array(int arr[], int n) {
+
+    int i;
+
+    for (i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
+}
