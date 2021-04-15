9bc1a766b443b204e2976674dc3ae799602b1326
Your Name <irshalfir@gmail.com>
2021-04-15 17:45:10 +0700

    add block file writer program

---
+++ university-hands-on/s2/4-files/blk-prg-wrt.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    FILE *fl;
+    char ans;
+    
+    struct clg_dt
+    {
+        char title[25];
+        char author[20];
+        int amount;
+    } book;
+
+    fl = fopen("fls/daftar-buku.dat", "wb");
+
+    if (fl == NULL) 
+    {
+        puts("file gagal diciptakan!!");
+        exit(1);
+    }
+
+    do
+    {
+        printf("Judul Buku      = "); gets(book.title);
+        printf("Nama Pengarang  = "); gets(book.author);
+        printf("Jumlah Buku     = "); scanf("%d", &book.amount);
+
+        fwrite(&book, sizeof(book), 1, fl);
+
+        printf("\nAda data buku lagi? (Y/T) ");
+        scanf(" %c", &ans);
+    } while (ans == 'Y' || ans == 'y');
+
+    fclose(fl);
+
+    return 0;
+}
